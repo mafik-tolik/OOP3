cfb3dc723271c3681c17cceb87332d106867d476
mafik_tolik <mafik_tolik@mail.ru>
2023-02-26 17:12:52 +0300

    Commit 1

---
+++ Ex3/Persons.java
@@ -0,0 +1,29 @@
+public class Persons {
+
+    enum Gender {
+        man, woman
+    }
+
+    protected String name;
+    protected Gender gender;
+    protected int age;
+
+    public Persons(String name, Persons.Gender gender, int age) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    protected int getAge() {
+        return age;
+    }
+
+    protected void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "name=" + name + ", gender=" + gender + ", age=" + age;
+    }
+}
